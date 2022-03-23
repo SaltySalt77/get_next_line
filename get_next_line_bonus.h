d6d3f5293de5def9b1c463d656af06e5ad1f172b
hyna <alexisna@naver.com>
2022-03-24 01:29:40 +0900

    [add]bonus files

---
+++ get_next_line_bonus.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyna <hyns@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 08:36:51 by hyna              #+#    #+#             */
+/*   Updated: 2022/03/05 17:12:42 by hyna             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyna <hyns@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/06 23:41:43 by hyna              #+#    #+#             */
+/*   Updated: 2022/03/05 17:18:41 by hyna             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char	*s, int c)
+{
+	char	tmp;
+
+	if (!s)
+		return (NULL);
+	tmp = (char)c;
+	while ((*s) && (*s != tmp))
+		s++;
+	if (*s == tmp)
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char	*s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
